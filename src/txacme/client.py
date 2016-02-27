1247762295a44c2d06b51d8f361727325cb2fa9e
Tristan Seligmann <mithrandi@mithrandi.net>
2016-02-28 00:38:04 +0200

    Registration works now, theoretically.

--- src/txacme/client.py
+++ src/txacme/client.py
@@ -5,8 +5,9 @@ import re
 
 from acme import errors, jose, jws, messages
 from treq.client import HTTPClient
-from twisted.internet.defer import succeed
+from twisted.internet.defer import inlineCallbacks, returnValue, succeed
 from twisted.logger import Logger
+from twisted.python.url import URL
 from twisted.web.client import Agent, HTTPConnectionPool
 from twisted.web.http_headers import Headers
 
@@ -14,24 +15,24 @@ from twisted.web.http_headers import Headers
 logger = Logger()
 
 
-def parse_header_links(value):
-    links = []
-    replace_chars = " '\""
-    for val in re.split(", *<", value):
+def _parse_header_links(value):
+    links = {}
+    replace_chars = b' \'"'
+    for val in re.split(b', *<', value):
         try:
             url, params = val.split(";", 1)
         except ValueError:
             url, params = val, ''
 
         link = {}
-        link["url"] = url.strip("<> '\"")
-        for param in params.split(";"):
+        link['url'] = url.strip('<> \'"')
+        for param in params.split(';'):
             try:
-                key, value = param.split("=")
+                key, value = param.split('=')
             except ValueError:
                 break
             link[key.strip(replace_chars)] = value.strip(replace_chars)
-        links.append(link)
+        links[link.get('rel') or link.get('url')] = link
     return links
 
 
@@ -48,6 +49,7 @@ class Client(object):
         else:
             self._client = JWSClient(treq_client, key, alg)
         self.directory = directory
+        self._key = key
 
     def register(self, new_reg=None):
         """
@@ -64,29 +66,54 @@ class Client(object):
 
         d = self._client.post(self.directory[new_reg], new_reg)
         d.addCallback(self._parse_regr_response)
+        d.addCallback(self._check_regr, new_reg)
         return d
 
-    def _parse_regr_response(self, response):
-        link = response.headers.getRawHeaders('link', [''])[0]
-        links = parse_header_links(link)
+    def _parse_regr_response(self, response, uri=None, new_authzr_uri=None,
+                             terms_of_service=None):
+        link = response.headers.getRawHeaders(b'link', [''])[0]
+        links = _parse_header_links(link)
         if 'terms-of-service' in links:
-            terms_of_service = links['terms-of-service']['url']
-        if 'next' in response.links:
-            new_authzr_uri = response.links['next']['url']
+            terms_of_service = URL.fromText(
+                links[b'terms-of-service'][b'url'].decode('ascii'))
+        if 'next' in links:
+            new_authzr_uri = URL.fromText(
+                links[b'next'][b'url'].decode('ascii'))
         if new_authzr_uri is None:
             raise errors.ClientError('"next" link missing')
+        location = response.headers.getRawHeaders('location', [None])[0]
+        if location is None:
+            location = uri
+        else:
+            location = URL.fromText(location.decode('ascii'))
+        return (
+            response.json()
+            .addCallback(
+                lambda body:
+                messages.RegistrationResource(
+                    body=messages.Registration.from_json(body),
+                    uri=location,
+                    new_authzr_uri=new_authzr_uri,
+                    terms_of_service=terms_of_service))
+            )
+
+    def _check_regr(self, regr, new_reg):
+        if (regr.body.key != self._key.public_key() or
+                regr.body.contact != new_reg.contact):
+            raise errors.UnexpectedUpdate(regr)
+        return regr
 
 
-JSON_CONTENT_TYPE = 'application/json'
-JSON_ERROR_CONTENT_TYPE = 'application/problem+json'
-REPLAY_NONCE_HEADER = 'Replay-Nonce'
+JSON_CONTENT_TYPE = b'application/json'
+JSON_ERROR_CONTENT_TYPE = b'application/problem+json'
+REPLAY_NONCE_HEADER = b'Replay-Nonce'
 
 
 class JWSClient(object):
     """
     HTTP client using JWS messaging.
     """
-    def __init__(self, treq_client, key, alg, user_agent='txacme'):
+    def __init__(self, treq_client, key, alg, user_agent=b'txacme'):
         self._treq = treq_client
         self._key = key
         self._alg = alg
@@ -97,11 +124,13 @@ class JWSClient(object):
     def _wrap_in_jws(self, obj, nonce):
         """Wrap `JSONDeSerializable` object in JWS.
 
-        .. todo:: Implement ``acmePath``.
+        ..  todo:: Implement ``acmePath``.
 
-        :param .JSONDeSerializable obj:
+        :param ~acme.jose.interfaces.JSONDeSerializable obj:
         :param bytes nonce:
-        :rtype: `.JWS`
+
+        :rtype: bytes
+        :return: JSON-encoded data
         """
         jobj = obj.json_dumps().encode()
         logger.debug('Serialized JSON: {jobj}', jobj=jobj)
@@ -111,39 +140,37 @@ class JWSClient(object):
             .json_dumps())
 
     @classmethod
+    @inlineCallbacks
     def _check_response(cls, response, content_type=None):
-        """Check response content and its type.
+        """
+        Check response content and its type.
         .. note::
            Checking is not strict: wrong server response ``Content-Type``
            HTTP header is ignored if response is an expected JSON object
            (c.f. Boulder #56).
-        :param str content_type: Expected Content-Type response header.
+        :param bytes content_type: Expected Content-Type response header.
             If JSON is expected and not present in server response, this
             function will raise an error. Otherwise, wrong Content-Type
             is ignored, but logged.
-        :raises .messages.Error: If server response body
+        :raises acme.messages.Error: If server response body
             carries HTTP Problem (draft-ietf-appsawg-http-problem-00).
-        :raises .ClientError: In case of other networking errors.
+        :raises acme.errors.ClientError: In case of other networking errors.
         """
         logger.debug('Received response {response} '
                      '(headers: {response.headers}): {response.content!r}',
                      response=response)
 
         response_ct = response.headers.getRawHeaders(
-            'Content-Type', [None])[0]
+            b'Content-Type', [None])[0]
         try:
             # TODO: response.json() is called twice, once here, and
             # once in _get and _post clients
-            jobj = response.json()
+            jobj = yield response.json()
         except ValueError:
             jobj = None
 
-        if not response.ok:
-            if jobj is not None:
-                if response_ct != JSON_ERROR_CONTENT_TYPE:
-                    logger.debug(
-                        'Ignoring wrong Content-Type ({res!r}) for JSON Error',
-                        res=response_ct)
+        if 400 <= response.code < 600:
+            if response_ct == JSON_ERROR_CONTENT_TYPE and jobj is not None:
                 try:
                     raise messages.Error.from_json(jobj)
                 except jose.DeserializationError as error:
@@ -158,44 +185,46 @@ class JWSClient(object):
                     'Ignoring wrong Content-Type ({res!r}) for JSON decodable '
                     'response', res=response_ct)
 
-            if content_type == JSON_CONTENT_TYPE and jobj is None:
+            if (content_type == JSON_CONTENT_TYPE) != (jobj is not None):
                 raise errors.ClientError(
-                    'Unexpected response Content-Type: {0}'.format(response_ct))
+                    'Unexpected response Content-Type: {0}'.format(
+                        response_ct))
 
-        return response
+        returnValue(response)
 
     def _send_request(self, method, url, *args, **kwargs):
-        """Send HTTP request.
+        """
+        Send HTTP request.
 
-        :param str method: method for the new `requests.Request` object
-        :param str url: URL for the new `requests.Request` object
-        :raises requests.exceptions.RequestException: in case of any problems
-        :returns: HTTP Response
-        :rtype: `requests.Response`
+        :param unicode method: The HTTP method to use.
+        :param twisted.python.url.URL url: The URL to make the request to.
+
+        :returns: Deferred firing with the HTTP Response
         """
         logger.debug('Sending {method} request to {url}. '
                      'args: {args!r}, kwargs: {kwargs!r}',
                      method=method, url=url, args=args, kwargs=kwargs)
         headers = kwargs.setdefault('headers', Headers())
-        if not headers.hasHeader('user-agent'):
-            headers.setRawHeaders('user-agent', [self._user_agent])
-        response = self._treq.request(method, url, *args, **kwargs)
-        #logger.debug('Received %s. Headers: %s. Content: %r',
-        #              response, response.headers, response.content)
+        if not headers.hasHeader(b'user-agent'):
+            headers.setRawHeaders(b'user-agent', [self._user_agent])
+        response = self._treq.request(
+            method, url.asText(), *args, **kwargs)
         return response
 
     def head(self, *args, **kwargs):
-        """Send HEAD request without checking the response.
-        Note, that `_check_response` is not called, as it is expected
+        """
+        Send HEAD request without checking the response.
+
+        Note that `_check_response` is not called, as it is expected
         that status code other than successfully 2xx will be returned, or
         messages2.Error will be raised by the server.
         """
-        return self._send_request(b'HEAD', *args, **kwargs)
+        return self._send_request(u'HEAD', *args, **kwargs)
 
     def get(self, url, content_type=JSON_CONTENT_TYPE, **kwargs):
         """Send GET request and check response."""
         return (
-            self._send_request(b'GET', url, **kwargs)
+            self._send_request(u'GET', url, **kwargs)
             .addCallback(self._check_request, content_type=content_type))
 
     def _add_nonce(self, response):
@@ -203,20 +232,25 @@ class JWSClient(object):
             REPLAY_NONCE_HEADER, [None])[0]
         if nonce is not None:
             try:
-                decoded_nonce = jws.Header._fields['nonce'].decode(nonce)
+                decoded_nonce = jws.Header._fields['nonce'].decode(
+                    nonce.decode('ascii'))
             except jose.DeserializationError as error:
                 raise errors.BadNonce(nonce, error)
             logger.debug('Storing nonce: {nonce!r}', nonce=decoded_nonce)
             self._nonces.add(decoded_nonce)
+            return response
         else:
             raise errors.MissingNonce(response)
 
     def _get_nonce(self, url):
-        d = succeed(None)
-        d.addCallback(lambda _: self.head(url))
-        d.addCallback(self._add_nonce)
-        d.addCallback(lambda _: self._nonces.pop())
-        return d
+        if len(self._nonces) > 0:
+            return succeed(self._nonces.pop())
+        else:
+            return (
+                self.head(url)
+                .addCallback(self._add_nonce)
+                .addCallback(lambda _: self._nonces.pop())
+                )
 
     def post(self, url, obj, content_type=JSON_CONTENT_TYPE, **kwargs):
         """POST object wrapped in `.JWS` and check response."""
@@ -224,7 +258,8 @@ class JWSClient(object):
             self._get_nonce(url)
             .addCallback(lambda nonce: self._wrap_in_jws(obj, nonce))
             .addCallback(
-                lambda data: self._send_request(b'POST', url, data=data, **kwargs))
+                lambda data: self._send_request(
+                    u'POST', url, data=data, **kwargs))
             .addCallback(self._add_nonce)
             .addCallback(
                 lambda response:
--- src/txacme/test/test_client.py
+++ src/txacme/test/test_client.py
@@ -1,17 +1,24 @@
-from functools import partial
-from operator import attrgetter
+import json
+from contextlib import contextmanager
+from operator import attrgetter, methodcaller
 
-from acme import jose, messages
-from acme.errors import ClientError
+from acme import jose, jws, messages, errors
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives.asymmetric import rsa
 from fixtures import Fixture
 from testtools import TestCase
-from testtools.matchers import AfterPreprocessing, IsInstance, Mismatch
-from testtools.twistedsupport import failed
+from testtools.matchers import (
+    AfterPreprocessing, Equals, IsInstance, MatchesAll, MatchesListwise,
+    MatchesPredicate, MatchesStructure, Mismatch)
+from testtools.twistedsupport import failed, succeeded
 from treq.client import HTTPClient
+from treq.testing import RequestSequence as treq_RequestSequence
 from treq.testing import (
-    _SynchronousProducer, HasHeaders, RequestSequence, RequestTraversalAgent,
+    _SynchronousProducer, HasHeaders, RequestTraversalAgent,
     StringStubbingResource)
 from twisted.internet import reactor
+from twisted.python.url import URL
+from twisted.web import http
 
 from txacme.client import Client
 from txacme.util import generate_private_key
@@ -19,6 +26,49 @@ from txacme.util import generate_private_key
 
 def failed_with(matcher):
     return failed(AfterPreprocessing(attrgetter('value'), matcher))
+
+
+# from cryptography:
+
+RSA_KEY_512 = jose.JWKRSA(key=rsa.RSAPrivateNumbers(
+    p=int(
+        "d57846898d5c0de249c08467586cb458fa9bc417cdf297f73cfc52281b787cd9", 16
+    ),
+    q=int(
+        "d10f71229e87e010eb363db6a85fd07df72d985b73c42786191f2ce9134afb2d", 16
+    ),
+    d=int(
+        "272869352cacf9c866c4e107acc95d4c608ca91460a93d28588d51cfccc07f449"
+        "18bbe7660f9f16adc2b4ed36ca310ef3d63b79bd447456e3505736a45a6ed21", 16
+    ),
+    dmp1=int(
+        "addff2ec7564c6b64bc670d250b6f24b0b8db6b2810099813b7e7658cecf5c39", 16
+    ),
+    dmq1=int(
+        "463ae9c6b77aedcac1397781e50e4afc060d4b216dc2778494ebe42a6850c81", 16
+    ),
+    iqmp=int(
+        "54deef8548f65cad1d411527a32dcb8e712d3e128e4e0ff118663fae82a758f4", 16
+    ),
+    public_numbers=rsa.RSAPublicNumbers(
+        e=65537,
+        n=int(
+            "ae5411f963c50e3267fafcf76381c8b1e5f7b741fdb2a544bcf48bd607b10c991"
+            "90caeb8011dc22cf83d921da55ec32bd05cac3ee02ca5e1dbef93952850b525",
+            16
+        ),
+    )
+).private_key(default_backend()))
+
+
+class Always(object):
+    """Always matches."""
+
+    def __str__(self):
+        return 'Always()'
+
+    def match(self, value):
+        return None
 
 
 class Never(object):
@@ -42,18 +92,17 @@ class ClientFixture(Fixture):
         if directory is None:
             self._directory = messages.Directory({
                 messages.NewRegistration:
-                'https://www.letsencrypt-demo.org/acme/new-reg',
+                URL.fromText(u'https://example.org/acme/new-reg'),
                 messages.Revocation:
-                'https://www.letsencrypt-demo.org/acme/revoke-cert',
+                URL.fromText(u'https://example.org/acme/revoke-cert'),
                 messages.NewAuthorization:
-                'https://www.letsencrypt-demo.org/acme/new-authz',
+                URL.fromText(u'https://example.org/acme/new-authz'),
                 })
         else:
             self._directory = directory
         if key is None:
-            self._key = jose.JWKRSA(key=generate_private_key('rsa'))
-        else:
-            self._key = key
+            key = jose.JWKRSA(key=generate_private_key('rsa'))
+        self._key = key
         self._alg = alg
 
     def _setUp(self):  # noqa
@@ -66,6 +115,72 @@ class ClientFixture(Fixture):
             treq_client=treq_client)
 
 
+def _nonce_response(url, nonce):
+    """
+    Construct an expected request for an initial nonce check.
+
+    :param bytes url: The url being requested.
+    :param bytes nonce: The nonce to return.
+
+    :return: A request/response tuple suitable for use with
+        :class:`treq.testing.RequestSeuence`.
+    """
+    return (
+        MatchesListwise([
+            Equals(b'HEAD'),
+            Equals(url),
+            Equals({}),
+            Equals(HasHeaders({b'user-agent': [b'txacme']})),
+            Equals(b'')]),
+        (http.NOT_ALLOWED,
+         {b'content-type': [b'application/json'],
+          b'replay-nonce': [jose.b64encode(nonce)]},
+         b'{}'))
+
+
+class RequestSequence(treq_RequestSequence):
+    @contextmanager
+    def consume(self, sync_failure_reporter):
+        yield
+        if not self.consumed():
+            sync_failure_reporter("\n".join(
+                ["Not all expected requests were made.  Still expecting:"] +
+                ["- {0!r})".format(e) for e, _ in self._sequence]))
+
+    def __call__(self, method, url, params, headers, data):
+        """
+        :return: the next response in the sequence, provided that the
+            parameters match the next in the sequence.
+        """
+        req = (method, url, params, headers, data)
+        if len(self._sequence) == 0:
+            self._async_reporter(
+                None, Never(),
+                "No more requests expected, but request {0!r} made.".format(
+                    req))
+            return (500, {}, "StubbingError")
+        matcher, response = self._sequence[0]
+        self._async_reporter(req, matcher)
+        self._sequence = self._sequence[1:]
+        return response
+
+
+def on_json(matcher):
+    return AfterPreprocessing(json.loads, matcher)
+
+
+def on_jws(matcher):
+    return on_json(
+        AfterPreprocessing(
+            jws.JWS.from_json,
+            MatchesAll(
+                MatchesPredicate(
+                    methodcaller('verify'), 'JWS message does not verify'),
+                AfterPreprocessing(
+                    attrgetter('payload'),
+                    on_json(matcher)))))
+
+
 class ClientTests(TestCase):
     """
     :class:`.Client` provides a client interface for the ACME API.
@@ -76,14 +191,117 @@ class ClientTests(TestCase):
         :exc:`~acme.errors.ClientError` failure occurs.
         """
         sequence = RequestSequence(
-            [((b'head',
-               b'https://www.letsencrypt-demo.org/acme/new-reg',
-               {},
-               HasHeaders({'user-agent': ['txacme']}),
-               b''),
-              (0, {}, b''))],
-            partial(self.expectThat, None, Never()))
-        client = self.useFixture(ClientFixture(sequence, None, None)).client
+            [_nonce_response(
+                u'https://example.org/acme/new-reg',
+                b'Nonce'),
+             (MatchesListwise([
+                 Equals(b'POST'),
+                 Equals(u'https://example.org/acme/new-reg'),
+                 Equals({}),
+                 Always(),
+                 Always()]),
+              (http.CREATED,
+               {b'content-type': [b'application/json'],
+                b'replay-nonce': [jose.b64encode(b'Nonce2')]},
+               b'{}'))],
+            self.expectThat)
+        client = self.useFixture(ClientFixture(sequence)).client
         with sequence.consume(self.fail):
             d = client.register()
-        self.assertThat(d, failed_with(IsInstance(ClientError)))
+        self.expectThat(
+            d, failed_with(MatchesAll(
+                IsInstance(errors.ClientError),
+                AfterPreprocessing(str, Equals('"next" link missing')))))
+
+    def test_unexpected_update(self):
+        """
+        If the server does not return the registration we expected, an
+        :exc:`~acme.errors.UnexpectedUpdate` failure occurs.
+        """
+        sequence = RequestSequence(
+            [_nonce_response(
+                u'https://example.org/acme/new-reg',
+                b'Nonce'),
+             (MatchesListwise([
+                 Equals(b'POST'),
+                 Equals(u'https://example.org/acme/new-reg'),
+                 Equals({}),
+                 Always(),
+                 Always()]),
+              (http.CREATED,
+               {b'content-type': b'application/json',
+                b'replay-nonce': jose.b64encode(b'Nonce2'),
+                b'location': b'https://example.org/acme/reg/1',
+                b'link': b','.join([
+                    b'<https://example.org/acme/new-authz>;rel="next"',
+                    b'<https://example.org/acme/recover-reg>;rel="recover"',
+                    b'<https://example.org/acme/terms>;rel="terms-of-service"',
+                ])},
+               json.dumps({
+                   u'key': {
+                       u'n': u'alQR-WPFDjJn-vz3Y4HIseX3t0H9sqVEvPSL1gexDJkZDK6'
+                             u'4AR3CLPg9kh2lXsMr0FysPuAspeHb75OVKFC1JQ',
+                       u'e': u'AQAB',
+                       u'kty': u'RSA'},
+                   u'contact': [u'mailto:example@example.com'],
+               })))],
+            self.expectThat)
+        client = self.useFixture(
+            ClientFixture(sequence, key=RSA_KEY_512)).client
+        reg = messages.NewRegistration.from_data(email=u'example@example.com')
+        with sequence.consume(self.fail):
+            d = client.register(reg)
+            self.assertThat(
+                d, failed_with(IsInstance(errors.UnexpectedUpdate)))
+
+    def test_register(self):
+        """
+        If the registration succeeds, the new registration is returned.
+        """
+        sequence = RequestSequence(
+            [_nonce_response(
+                u'https://example.org/acme/new-reg',
+                b'Nonce'),
+             (MatchesListwise([
+                 Equals(b'POST'),
+                 Equals(u'https://example.org/acme/new-reg'),
+                 Equals({}),
+                 Always(),
+                 on_jws(Equals({
+                     u'resource': u'new-reg',
+                     u'contact': [u'mailto:example@example.com']}))]),
+              (http.CREATED,
+               {b'content-type': b'application/json',
+                b'replay-nonce': jose.b64encode(b'Nonce2'),
+                b'location': b'https://example.org/acme/reg/1',
+                b'link': b','.join([
+                    b'<https://example.org/acme/new-authz>;rel="next"',
+                    b'<https://example.org/acme/recover-reg>;rel="recover"',
+                    b'<https://example.org/acme/terms>;rel="terms-of-service"',
+                ])},
+               json.dumps({
+                   u'key': {
+                       u'n': u'rlQR-WPFDjJn-vz3Y4HIseX3t0H9sqVEvPSL1gexDJkZDK6'
+                             u'4AR3CLPg9kh2lXsMr0FysPuAspeHb75OVKFC1JQ',
+                       u'e': u'AQAB',
+                       u'kty': u'RSA'},
+                   u'contact': [u'mailto:example@example.com'],
+               })))],
+            self.expectThat)
+        client = self.useFixture(
+            ClientFixture(sequence, key=RSA_KEY_512)).client
+        reg = messages.NewRegistration.from_data(email=u'example@example.com')
+        with sequence.consume(self.fail):
+            d = client.register(reg)
+            self.assertThat(
+                d, succeeded(MatchesStructure(
+                    body=MatchesStructure(
+                        key=Equals(RSA_KEY_512.public_key()),
+                        contact=Equals(reg.contact)),
+                    uri=Equals(
+                        URL.fromText(u'https://example.org/acme/reg/1')),
+                    new_authzr_uri=Equals(
+                        URL.fromText(u'https://example.org/acme/new-authz')),
+                    terms_of_service=Equals(
+                        URL.fromText(u'https://example.org/acme/terms')),
+                )))
