78437826994dd2da51c4070253b3a47b05114d7f
Tristan Seligmann <mithrandi@mithrandi.net>
2016-02-27 18:28:09 +0200

    Give up on Twisted 14.0.2 to get the new logger.

--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ setup(
         'Topic :: Software Development :: Libraries :: Python Modules',
         ],
     install_requires=[
-        'Twisted[tls]>=14.0.2',
+        'Twisted[tls]>=15.2.0',
         'acme>=0.4.0',
         'treq>=15.1.0',
         ],
