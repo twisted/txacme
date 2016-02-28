26649e7b07aafddfd9a4e0ad63dc66117f1480e6
Tristan Seligmann <mithrandi@mithrandi.net>
2016-02-28 07:39:52 +0200

    More coverage.

--- src/txacme/test/test_client.py
+++ src/txacme/test/test_client.py
@@ -178,8 +178,7 @@ class RequestSequence(treq_RequestSequence):
 
 def on_json(matcher):
     def _loads(s):
-        if isinstance(s, bytes):
-            s = s.decode('utf-8')
+        s = s.decode('utf-8')
         return json.loads(s)
     return AfterPreprocessing(_loads, matcher)
 
