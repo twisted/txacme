75c0e7061e52797a94f6645d01b0d6d58c5f824e
Tristan Seligmann <mithrandi@mithrandi.net>
2016-03-11 04:47:27 +0200

    Make "light" a little heavier.

--- src/txacme/test/__init__.py
+++ src/txacme/test/__init__.py
@@ -6,7 +6,7 @@ from hypothesis import settings
 eliot.twisted.redirectLogsForTrial()
 del eliot
 
-settings.register_profile("light", settings(max_examples=10))
+settings.register_profile("light", settings(max_examples=20))
 settings.register_profile("coverage", settings(max_examples=0))
 settings.load_profile(getenv(u'HYPOTHESIS_PROFILE', 'default'))
 del settings, getenv
